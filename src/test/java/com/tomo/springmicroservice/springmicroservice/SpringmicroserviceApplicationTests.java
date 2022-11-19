988375503245ca508b430df91606b0ec026f6ce1
tomo2020jp <39954318+tomo2020jp@users.noreply.github.com>
2022-11-19 16:47:19 +0900

    init project

---
+++ src/test/java/com/tomo/springmicroservice/springmicroservice/SpringmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomo.springmicroservice.springmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
