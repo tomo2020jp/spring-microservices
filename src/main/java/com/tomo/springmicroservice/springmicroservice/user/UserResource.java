85e62132cabe545f6bbbb082da1e74d5c92b52bb
tomo2020jp <39954318+tomo2020jp@users.noreply.github.com>
2022-11-19 17:24:26 +0900

    feat: return http status code

--- src/main/java/com/tomo/springmicroservice/springmicroservice/user/UserResource.java
+++ src/main/java/com/tomo/springmicroservice/springmicroservice/user/UserResource.java
@@ -1,29 +1,39 @@
 package com.tomo.springmicroservice.springmicroservice.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 public class UserResource {
     private final UserDaoService service;
 
-    public UserResource(UserDaoService service){
+    public UserResource(UserDaoService service) {
         this.service = service;
     }
 
     @GetMapping("/users")
-    public List<User> retrieveAllUsers(){
+    public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
-        service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
