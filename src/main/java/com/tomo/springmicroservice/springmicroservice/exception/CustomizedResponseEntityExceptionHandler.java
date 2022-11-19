515e4f422f5923bec38e5e260c76e80876fed90e
tomo2020jp <39954318+tomo2020jp@users.noreply.github.com>
2022-11-19 17:47:25 +0900

    feat: add validation

--- src/main/java/com/tomo/springmicroservice/springmicroservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/tomo/springmicroservice/springmicroservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,16 +1,20 @@
 package com.tomo.springmicroservice.springmicroservice.exception;
 
 import com.tomo.springmicroservice.springmicroservice.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.websocket.OnClose;
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class CustomizedResponseEntityExceptionHandler {
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception {
@@ -24,5 +28,16 @@ public class CustomizedResponseEntityExceptionHandler {
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
                 ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request
+    ) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                "Total Errors:" + ex.getErrorCount() + " First Error:" + ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/tomo/springmicroservice/springmicroservice/user/User.java
+++ src/main/java/com/tomo/springmicroservice/springmicroservice/user/User.java
@@ -4,11 +4,16 @@ import lombok.Data;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 @Data
 public class User {
 
     private Integer id;
+    @Size(min=2,message = "Name should have at least 2 characters")
     private String name;
+    @Past(message = "Birth Date should be in the past")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
